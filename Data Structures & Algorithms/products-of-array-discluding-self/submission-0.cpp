63bbdb62041d1511e5e997f053a73caa1fd225c0
Smruti Surekha Sahoo <87087088+smrutisurekha9090@users.noreply.github.com>
2026-05-06 13:22:31 +0530

    Add: products-of-array-discluding-self - submission-0

---
+++ Data Structures & Algorithms/products-of-array-discluding-self/submission-0.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    //Brute force
+    vector<int> productExceptSelf(vector<int>& nums) {
+        vector<int>ans;
+        for(int i=0;i<nums.size();i++){
+            int prod=1;
+            for(int j=0;j<nums.size();j++){
+                if(i != j){
+                    prod *=nums[j];
+                }
+            }
+            ans.push_back(prod);
+        }
+
+        return ans;  
+    }
+};
