f7ed1881feee7f65f0021fe18722a835d10324fa
Smruti Surekha Sahoo <87087088+smrutisurekha9090@users.noreply.github.com>
2026-05-11 21:19:21 +0530

    Add: sliding-window-maximum - submission-3

---
+++ Data Structures & Algorithms/sliding-window-maximum/submission-3.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+        vector<int> ans;
+        list<int>l;
+        int i=0;
+        int j=0;
+        while(j< nums.size()){
+            while(l.size()>0 && nums[j] > l.back()){
+                l.pop_back();
+            }
+            l.push_back(nums[j]);
+            if(j-i+1 < k){
+                j++;
+            }
+            else if(j-i+1 == k){
+                ans.push_back(l.front());
+                if(l.front()==nums[i]){
+                    l.pop_front();
+                }
+                i++;
+                j++;
+            }
+        }
+        return ans;
+
+    }
+};
