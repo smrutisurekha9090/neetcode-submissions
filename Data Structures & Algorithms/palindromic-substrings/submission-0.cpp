ad5cde414bf8be94b58d0b3da7d031826b98c1f3
Smruti Surekha Sahoo <87087088+smrutisurekha9090@users.noreply.github.com>
2026-05-21 20:02:05 +0530

    Add: palindromic-substrings - submission-0

---
+++ Data Structures & Algorithms/palindromic-substrings/submission-0.cpp
@@ -0,0 +1,23 @@
+class Solution {
+   public:
+    int expand(string& s, int left, int right) {
+        int count = 0;
+        while (left >= 0 && right < s.size() && s[left] == s[right]) {
+            count++;
+            left--;
+            right++;
+        }
+        return count;
+    }
+    int countSubstrings(string s) {
+        int n = s.size();
+        int ans = 0;
+        for (int i = 0; i < n; i++) {
+            // odd
+            ans += expand(s, i, i);
+            // even
+            ans += expand(s, i, i + 1);
+        }
+        return ans;
+    }
+};
