4486f42d1c0b48907c1f5af64f738e663d70f81d
Smruti Surekha Sahoo <87087088+smrutisurekha9090@users.noreply.github.com>
2026-05-24 02:12:23 +0530

    Add: house-robber-ii - submission-0

---
+++ Data Structures & Algorithms/house-robber-ii/submission-0.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int solve(int i,int end , vector<int>& nums,vector<int>&dp ){
+        int n = nums.size();
+        if(i > end) return 0;
+        if(dp[i] != -1) return dp[i];
+        int rob = nums[i]+ solve(i+2,end,nums,dp);
+        int skip =  solve(i+1,end,nums,dp);
+        return dp[i]=max(rob,skip);
+    }
+    int rob(vector<int>& nums) {
+        int n = nums.size();
+        if(n == 1) return nums[0];
+
+        vector<int>dp1(n+1,-1);
+        int case1 = solve(0,n-2,nums,dp1);
+
+        vector<int>dp2(n+1,-1);
+        int case2 = solve(1,n-1,nums,dp2);
+
+        return max(case1,case2);
+    }
+};
