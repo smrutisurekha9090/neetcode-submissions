fc1cf40ed165d85105b3834d582745fb4f222c0a
Smruti Surekha Sahoo <87087088+smrutisurekha9090@users.noreply.github.com>
2026-05-27 16:51:47 +0530

    Add: subsets - submission-1

---
+++ Data Structures & Algorithms/subsets/submission-1.cpp
@@ -0,0 +1,24 @@
+class Solution {
+   public:
+    vector<vector<int>> ans;
+    void solve(vector<int> ip, vector<int> op) {
+        if (ip.size() == 0) {
+            ans.push_back(op);
+            return;
+        }
+        vector<int> op1 = op;
+        vector<int> op2 = op;
+
+        op2.push_back(ip[0]);
+        ip.erase(ip.begin());
+        
+        solve(ip, op1);
+        solve(ip, op2);
+    }
+
+    vector<vector<int>> subsets(vector<int>& nums) {
+        vector<int> op;
+        solve(nums, op);
+        return ans;
+    }
+};
